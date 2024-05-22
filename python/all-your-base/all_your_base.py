d89f8b5b866caec74a5d65bc4eb451fc268614e1
Renato Alencar <renato.luko@yahoo.com>
2024-05-21 21:12:10 -0300

    sync with exercism

--- python/all-your-base/all_your_base.py
+++ python/all-your-base/all_your_base.py
@@ -1,14 +1,21 @@
 def rebase(input_base, digits, output_base):
-    print(f"input_base: {input_base}, \ndigits: {digits}, \noutput_base: {output_base}")
+    if input_base < 2:
+        raise ValueError("input base must be >= 2")
+    elif output_base < 2:
+        raise ValueError("output base must be >= 2")
+    elif not all(map(lambda x: 0 <= x < input_base, digits)):
+        raise ValueError("all digits must satisfy 0 <= d < input base")
+    elif digits == []:
+        return [0]
 
-    digitsArr = []
+    res, base10 = [], 0
 
-    for index, value in enumerate(str(digits)):
-        print(f"index: {index}, value: {value}")
-        i = index
-        digitsArr.append(f"({value} x {input_base}^{len(str(digits)) +(i - 1)})")
+    for index, value in enumerate(digits):
+        base10 += value * (input_base ** (len(digits) - (index + 1)))
+    if base10 <= output_base:
+        return [base10]
+    while base10 > 1:
+        res.insert(0, round(round((base10 % output_base) - 0.4)))
+        base10 = base10 / output_base
 
-    print(" + ".join(digitsArr))
-
-
-rebase(2, 101010, 4)
+    return res
