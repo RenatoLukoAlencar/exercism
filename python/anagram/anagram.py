772c9a0e6a56e49cdb84c3f0f9a30af96f422fff
Renato Alencar <renato.luko@yahoo.com>
2024-05-22 21:50:54 -0300

    new anagrama

--- python/anagram/anagram.py
+++ python/anagram/anagram.py
@@ -1,16 +1,7 @@
 def find_anagrams(word, candidates):
-    main_set, res = set(list(word.lower())), []
-
-    for item in candidates:
-        if item.lower() != word.lower() and len(word) == len(item):
-            if set(list(item.lower())) & main_set == main_set:
-                w = list(word.lower())
-
-                for letter in list(item.lower()):
-                    if w.count(letter) > 0:
-                        w.pop(w.index(letter))
-
-                if len(w) == 0:
-                    res.append(item)
+    res, sword = [], word.casefold()
+    for cand in candidates:
+        if sword != cand.casefold() and sorted(sword) == sorted(cand.casefold()):
+            res.append(cand)
 
     return res
