ecdf19cc4844246309b2563957724f9e98bd61cf
Renato Alencar <renato.luko@yahoo.com>
2024-05-25 18:45:46 -0300

    ended house

--- python/house/house.py
+++ python/house/house.py
@@ -1,9 +1,24 @@
 def recite(start_verse, end_verse):
-    splitter = " that "
-    poem = "is the horse and the hound and the horn that belonged to the farmer sowing his corn that kept the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.".split(
-        splitter
-    )
-    res = splitter.join(poem[-(start_verse + 1) :])
-    test = " ".join(poem[-start_verse].split(" ")[2:])
-    sub = res.split(" ")[2] if res.split(" ")[2] != "the" else ""
-    return [f"This is the {sub} {' '.join(res.split(' ')[3:])}".replace("  ", " ")]
+    poem = [
+        "",
+        "",
+        "malt that lay in the ",
+        "rat that ate the ",
+        "cat that killed the ",
+        "dog that worried the ",
+        "cow with the crumpled horn that tossed the ",
+        "maiden all forlorn that milked the ",
+        "man all tattered and torn that kissed the ",
+        "priest all shaven and shorn that married the ",
+        "rooster that crowed in the morn that woke the ",
+        "farmer sowing his corn that kept the ",
+        "horse and the hound and the horn that belonged to the ",
+    ]
+    song = []
+    for i in range(start_verse, end_verse + 1):
+        verse = ""
+        for j in range(i, 0, -1):
+            verse += poem[j]
+
+        song.append(f"This is the {verse}house that Jack built.")
+    return song
