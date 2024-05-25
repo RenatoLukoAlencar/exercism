fbc15bcaf3dac2c98a825af782f81ac3bdf38d64
Renato Alencar <renato.luko@yahoo.com>
2024-05-24 23:00:03 -0300

    minefield inconpleted

---
+++ python/minesweeper/minesweeper.py
@@ -0,0 +1,19 @@
+def annotate(minefield):
+    if len(minefield) == 1:
+        return [""]
+
+    try:
+        checkLenLines = []
+        for line in minefield:
+            checkLenLines.append(len(line))
+
+        if not all(checkLenLines):
+            raise Exception
+        mx = len(minefield)
+        my = len(minefield[0])
+        board = [" " for _ in range(my) for _ in range(mx)]
+        print(f"mx: {mx}, my: {my}")
+
+        return board
+    except:
+        raise ValueError("The board is invalid with current input.")
