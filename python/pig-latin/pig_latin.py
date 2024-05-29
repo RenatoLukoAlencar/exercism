76b343806b031d4999b5c2d9d4c91bc9de673d38
Renato Alencar <renato.luko@yahoo.com>
2024-05-28 21:54:30 -0300

    pig latin, need refactory

--- python/pig-latin/pig_latin.py
+++ python/pig-latin/pig_latin.py
@@ -1,35 +1,21 @@
 def translate(text):
-    arr = text.split(" ")
-    print(f"arr: {arr}")
-    vogalx = ["a", "e", "i", "o", "u", "x"]
-    consoants = [
-        "b",
-        "c",
-        "d",
-        "f",
-        "g",
-        "h",
-        "j",
-        "k",
-        "l",
-        "m",
-        "n",
-        "p",
-        "q",
-        "r",
-        "s",
-        "t",
-        "v",
-        "x",
-        "y",
-        "w",
-        "z",
-    ]
+    arr, vow = text.split(" "), "aeiou"
 
-    for index, value in enumerate(arr):
-        if value[0] in vogalx:
-            arr[index] = f"{value}{'ay'}"
+    for i, v in enumerate(arr):
+        if v[0] in vow or (v[0] in "yx" and v[1] in "tr"):
+            arr[i] = arr[i] + "ay"
+        elif v[1] == "y":
+            arr[i] = arr[i][1:] + arr[i][:1] + "ay"
+        elif (v[0] not in vow and v[1] not in vow and v[2] not in vow) or (
+            v[0] not in vow and v[1:3] == "qu"
+        ):
+            arr[i] = (
+                arr[i][v.index("y") :] + arr[i][: v.index("y")] + "ay"
+                if "y" in v[:3]
+                else arr[i][3:] + arr[i][:3] + "ay"
+            )
+        elif v[:2] == "qu" or v[1] not in vow:
+            arr[i] = arr[i][2:] + arr[i][:2] + "ay"
         else:
-            arr[index] = f"{value[1:]}{value[0]}ay"
-
+            arr[i] = arr[i][1:] + arr[i][:1] + "ay"
     return " ".join(arr)
