988a8a1605fbdb1fa7f1c676ce32379a0f83215e
Renato Alencar <renato.luko@yahoo.com>
2024-05-29 12:53:08 -0300

    sublist

---
+++ python/sublist/sublist.py
@@ -0,0 +1,26 @@
+"""
+This exercise stub and the test suite contain several enumerated constants.
+
+Enumerated constants can be done with a NAME assigned to an arbitrary,
+but unique value. An integer is traditionally used because it’s memory
+efficient.
+It is a common practice to export both constants and functions that work with
+those constants (ex. the constants in the os, subprocess and re modules).
+
+You can learn more here: https://en.wikipedia.org/wiki/Enumerated_type
+"""
+
+# Possible sublist categories.
+# Change the values as you see fit.
+SUBLIST = None
+SUPERLIST = None
+EQUAL = None
+UNEQUAL = None
+
+
+def sublist(list_one, list_two):
+    a = set(list_one)
+    b = set(list_two)
+    print(f"a: {a}\nb: {b}")
+
+    pass
