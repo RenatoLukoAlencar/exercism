a2e3f2a76c847035809133eec2f09539cbf92964
Renato Alencar <renato.luko@yahoo.com>
2024-05-30 19:41:58 -0300

    finish prime factors

--- python/prime-factors/prime_factors.py
+++ python/prime-factors/prime_factors.py
@@ -1,21 +1,9 @@
 def factors(value):
-    res = []
-
-    if value == 1:
-        return res
-
-    current = value
-    test = primes()
-    for i in test:
-        while current % i == 0:
-            res.append(i)
-            current /= i
-
-    return res
-
-
-def primes():  # simple sieve of multiples
-    n = 10000
-    odds = range(3, n + 1, 2)
-    sieve = set(sum([list(range(q * q, n + 1, q + q)) for q in odds], []))
-    return [2] + [p for p in odds if p not in sieve] + [894119]
+    factors, f = [], 2
+    while value > 1:
+        if value % f == 0:
+            factors.append(f)
+            value /= f
+        else:
+            f += 1
+    return factors
