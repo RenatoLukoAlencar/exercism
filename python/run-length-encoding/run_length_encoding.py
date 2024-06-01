d2fbaa839887918dbd9ee51e1d5bbfa1d3f60820
Renato Alencar <renato.luko@yahoo.com>
2024-06-01 19:40:31 -0300

    finish run length

--- python/run-length-encoding/run_length_encoding.py
+++ python/run-length-encoding/run_length_encoding.py
@@ -1,6 +1,21 @@
 def decode(string):
-    pass
+    res, count = "", ""
 
+    for letter in string:
+        if letter.isnumeric():
+            count += letter
+        else:
+            res += letter * int(count) if count != "" else letter
+            count = ""
+
+    return res
 
 def encode(string):
-    pass
+    count, ll, res = 1, "", ""
+
+    for letter in string + "#":
+        if ll != letter:
+            res += f"{count if count > 1 else ''}{ll}"
+            count, ll = 0, letter
+        count += 1
+    return res
