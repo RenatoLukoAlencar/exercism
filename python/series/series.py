d57000f537e254c57a18b5f25ed9e6fa4130827a
Renato Alencar <renato.luko@yahoo.com>
2024-06-01 18:31:11 -0300

    finish series

--- python/series/series.py
+++ python/series/series.py
@@ -1,2 +1,11 @@
 def slices(series, length):
-    pass
+    if length == 0:
+        raise ValueError("slice length cannot be zero")
+    elif length < 0:
+        raise ValueError("slice length cannot be negative")
+    elif len(series) == 0:
+        raise ValueError("series cannot be empty")
+    elif len(series) < length:
+        raise ValueError("slice length cannot be greater than series length")
+
+    return [series[i:i+length] for i in range(0, len(series)-length+1)]
