847d697e260e72b645e73f84cc32989daae843ca
Renato Alencar <renato.luko@yahoo.com>
2024-06-05 20:37:23 -0300

    finish dnd

--- python/dnd-character/dnd_character.py
+++ python/dnd-character/dnd_character.py
@@ -1,5 +1,19 @@
 import random
 
+ATT = ["strength", "dexterity", "constitution", "intelligence", "wisdom", "charisma"]
+
+
 class Character:
     def __init__(self):
-        pass
+        for attribute in ATT:
+            setattr(self, attribute, self.ability())
+
+        self.hitpoints = 10 + modifier(self.constitution)
+
+    def ability(self):
+        test = sorted([random.randint(1, 6) for _ in range(3)])
+        return sum(test[1:])
+
+
+def modifier(n):
+    return (n - 10) // 2
