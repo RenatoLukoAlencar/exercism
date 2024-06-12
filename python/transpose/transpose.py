40feac47df20032aaec1f999f10ae7cc03af02fb
Renato Alencar <renato.luko@yahoo.com>
2024-06-11 21:46:11 -0300

    transpose ongoing

--- python/transpose/transpose.py
+++ python/transpose/transpose.py
@@ -1,2 +1,18 @@
 def transpose(lines):
-    pass
+    print(f"lines: {lines}")
+    res = []
+
+    for index, item in enumerate(lines):
+        print(f"i: {index}, item: {item}")
+
+        if len(res) - 1 < index:
+            res.append([])
+
+        res[index].append(item)
+
+    for index, line in enumerate(res):
+        res[index] = "".join(line)
+
+    print(f"res: {res}")
+
+    return res
