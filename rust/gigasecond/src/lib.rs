c06e879fee1730461ac840fea48d16c0a03ed1a2
Renato Alencar <renato.luko@yahoo.com>
2024-06-13 00:05:15 -0300

    starting rust

--- rust/gigasecond/src/lib.rs
+++ rust/gigasecond/src/lib.rs
@@ -1,8 +1,9 @@
+use time::Duration;
 use time::PrimitiveDateTime as DateTime;
 
 // Returns a DateTime one billion seconds after start.
 pub fn after(start: DateTime) -> DateTime {
-    let mut giga_seconds = 1000000000;
+    let giga_seconds = 1000000000;
 
-    return start.checked_add(giga_seconds);
+    return start + Duration::seconds(giga_seconds);
 }
