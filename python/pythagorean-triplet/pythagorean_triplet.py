c9afd1bd83a1fa4030970913cf5f6c107b5ccc4a
Renato Alencar <renato.luko@yahoo.com>
2024-06-17 23:47:04 -0300

    in progress pythagorean triplet

--- python/pythagorean-triplet/pythagorean_triplet.py
+++ python/pythagorean-triplet/pythagorean_triplet.py
@@ -1,2 +1,9 @@
 def triplets_with_sum(number):
-    pass
+    res = []
+    for a in range(1, int(number / 3)):
+        for b in range(a + 1, int(number / 2)):
+            c = number - a - b
+            if a * a + b * b == c * c:
+                res.append([a, b, c])
+
+    return res
