f2cc61a95ce035180b9a168229920207e87920c7
Renato Alencar <renato.luko@yahoo.com>
2024-07-11 20:19:01 -0300

    ongoing rectangles

--- python/rectangles/rectangles.py
+++ python/rectangles/rectangles.py
@@ -1,2 +1,11 @@
-def rectangles(strings):
-    pass
+def rectangles(strings: list):
+    newList, count = [], 0
+    for s in strings:
+        newList.append(list(s))
+
+    for l, line in enumerate(newList):
+        for r, row in enumerate(line):
+            if newList[l][r] == "+":
+                count += 1
+
+    return count
