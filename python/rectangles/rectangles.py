3612bf5a2df1f3c1d65f7bccc026cadf4fa240c9
Renato Alencar <renato.luko@yahoo.com>
2024-07-13 19:16:15 -0300

    in progress rectangle

--- python/rectangles/rectangles.py
+++ python/rectangles/rectangles.py
@@ -4,8 +4,36 @@ def rectangles(strings: list):
         newList.append(list(s))
 
     for l, line in enumerate(newList):
-        for r, row in enumerate(line):
-            if newList[l][r] == "+":
-                count += 2
+        if l != len(newList) - 1:
+            twoOrMore = line.count("+") >= 2
 
+            if twoOrMore:
+                # print(f"line{l +1}: {line}")
+                # print(f"    this line contain {line.count("+")}")
+                lineIndexs = []
+                for c, col in enumerate(line):
+                    if col == "+":
+                        lineIndexs.append(c)
+
+                for startCorner in range(len(lineIndexs)):
+                    if startCorner != len(lineIndexs) - 1:
+                        segment = line[
+                            lineIndexs[startCorner] : lineIndexs[startCorner + 1] + 1
+                        ]
+                        if segment.count(" ") == 0:
+                            # print(f"      segment{startCorner}: {segment}")
+
+                            allseg = [str(segment)]
+                            seqRow = l + 1
+                            while l + seqRow <= len(newList) - 2:
+
+                                if (
+                                    newList[seqRow][0] == " "
+                                    or newList[seqRow][-1] == " "
+                                ):
+                                    break
+
+                                seqRow += 1
+
+                            count += 1
     return count
