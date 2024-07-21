5ba4ff14af374d11fd08be596210d28df88e94c9
Renato Alencar <renato.luko@yahoo.com>
2024-07-21 08:05:02 -0300

    ongoing - 11 of 14 tests

--- python/rectangles/rectangles.py
+++ python/rectangles/rectangles.py
@@ -1,28 +1,52 @@
 def rectangles(strings: list):
-    nL, count = [list(s) for s in strings], 0
+    for s in strings:
+        print(s)
+    count = 0
 
-    for row, line in enumerate(nL):
-        for column, vertice in enumerate(line):
-            for nextPoint in range(column + 1, len(line)):
-                if vertice == "+" and line[nextPoint] == "+":
-                    topSeg = line[column : nextPoint + 1]
+    for line in range(len(strings) - 1):
+        for col in range(len(strings[line]) - 1):
+            for end_line in range(line + 1, len(strings)):
+                for end_col in range(col + 1, len(strings[end_line])):
+                    s1 = strings[line][col : end_col + 1]
+                    #  print(f"s1: {list(s1)}")
+                    s2 = "".join(
+                        [l[end_col] for l in strings[line:]][line : end_line + 1]
+                    )
+                    #  print(f"s2: {list(s2)}")
+                    s3 = strings[end_line][col : end_col + 1]
+                    # print(f"s3: {list(s3)}")
+                    s4 = "".join([l[col] for l in strings[line:]][line : end_line + 1])
+                    # print(f"s4: {list(s4)}")
 
-                    if topSeg.count(" ") == 0:
-                        allSeg = [topSeg]
-                        for i in range(row, len(nL)):
-                            if nL[i][column] == " " or nL[i][nextPoint] == " ":
-                                break
-
-                            seg = nL[i][column : nextPoint + 1]
-                            allSeg.append(seg)
-
-                            if (
-                                seg[0] == "+"
-                                and seg[-1] == "+"
-                                and (seg.count(" ") == 0 and seg.count("|") == 0)
-                            ):
-                                count += 1
-
-                        for x in allSeg:
-                            print(f"xx: {x}")
+                    currentRect = [s1, s2, s3, s4]
+                    # print(f"currentRect\n {currentRect}")
+                    if validRectangle(currentRect):
+                        count += 1
     return count
+
+
+def validRectangle(sides):
+    valid, direction = True, "-"
+
+    for side in sides:
+        if side == "":
+            valid = False
+        elif not validSide(side, direction):
+            valid = False
+
+        direction = "|" if direction == "-" else "-"
+
+    return valid
+
+
+def validSide(side, direction):
+    valid, dirNotAllowed = True, "-" if direction == "|" else "|"
+
+    if side[0] != "+" or side[-1] != "+":
+        valid = False
+    elif side.count(dirNotAllowed) > 0:
+        valid = False
+    elif side.count(" ") > 0:
+        valid = False
+
+    return valid
